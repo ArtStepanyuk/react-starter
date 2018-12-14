8e822f3e9c358d98baabead2b71cebc46a8dff70
Artstepanyuk <lmasamunel1988@gmail.com>
2018-12-14 15:01:17 +0200

    minimal react parcel fontawesome reactstrap eslintrc config for quick start

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { fas } from '@fortawesome/free-solid-svg-icons'
+import { far } from '@fortawesome/free-regular-svg-icons'
+import { fab } from '@fortawesome/free-brands-svg-icons'
+import React from 'react'
+
+library.add(fas, far, fab)
+function App() {
+	return <div>hi</div>
+}
+
+export default App
